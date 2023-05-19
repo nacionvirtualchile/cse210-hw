8bf40ddd5e79376dfe14ee65267f3fca0174ce02
nacionvirtualchile <andrexrojas@hotmail.com>
2023-05-19 15:29:03 -0400

    190523-1

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learning02
+{
+    public class Job
+    {
+        // The C# convention is to start member variables with an underscore _
+        public string _companyName = "";
+        public string _jobTitle  = "";
+        public int _startYear;
+        public int _endYear;
+
+        // A special method, called a constructor that is invoked using the  
+        // new keyword followed by the class name and parentheses.
+        public void DisplayJobDetails()
+        {
+            Console.WriteLine(_jobTitle+" ("+_companyName+") "+_startYear+"-"+_endYear);
+
+        }
+
+        // A method that displays the person's full name as used in eastern 
+        // countries or <family name, given name>.
+
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Microsoft";
+        job1._startYear = 2000;
+        job1._endYear = 2010;
+        //Console.WriteLine(job1._companyName);
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._companyName = "Apple";
+        job2._startYear = 2000;
+        job2._endYear = 2010;
+        //Console.WriteLine(job2._companyName);
+        job2.DisplayJobDetails();
+        
     }
 }
